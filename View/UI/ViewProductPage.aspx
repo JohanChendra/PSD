183ee8bf7bd72275f64bc328ac567f86fc4140a1
JohanChendra <71887053+JohanChendra@users.noreply.github.com>
2020-09-26 13:26:01 +0700

    Add files via upload

---
+++ View/UI/ViewProductPage.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ViewProductPage.aspx.cs" Inherits="TokoBeDia.View.ViewProductPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>View Product Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button ID="HomeButton" runat="server" Text="Back To Home" OnClick="HomeButton_Click" />
+    </div>
+    <div>
+        <p>
+            Products
+        </p>
+    </div>
+    <asp:GridView ID="ProductsGridView" runat="server" OnRowDeleting="ProductsGridView_RowDeleting" OnRowUpdating="ProductsGridView_RowUpdating">
+        <Columns>
+            <asp:ButtonField ButtonType="Button" CommandName="Update" Text="Update" />
+            <asp:ButtonField ButtonType="Button" CommandName="Delete" Text="Delete" />
+        </Columns>
+        </asp:GridView>
+    <div>
+        <asp:Button ID="InsertProductButton" runat="server" Text="Insert Product" OnClick="InsertProductButton_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ View/UI/ViewUser.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TokoBeDia.Model;
+using TokoBeDia.Repository;
+
+namespace TokoBeDia.View
+{
+    public partial class ViewUser : System.Web.UI.Page
+    {
+        TokoBeDiaEntities db = new TokoBeDiaEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack == false)
+            {
+                SubmitButton.Visible = false;
+                LabelNewRole.Visible = false;
+                NewRoletxt.Visible = false;
+                LabelID.Visible = false;       
+                RefreshGridView();
+            }            
+        }
+
+        private void RefreshGridView()
+        {
+            UserGridView.DataSourceID = "UserDataSource";
+            UserGridView.DataBind();
+        }
+
+        protected void RoleButton_Click(object sender, EventArgs e)
+        {
+            int ID = Int32.Parse(Session["ID"].ToString());
+            int rowindex = Int32.Parse((sender as Button).CommandArgument);
+            LabelID.Text = rowindex.ToString();
+            User u = UserRepository.GetUserid(rowindex);
+            if (ID == rowindex)
+            {
+                Labelerror.Text = "Tidak bisa mengganti role diri sendiri";
+            }
+            else
+            {
+                LabelNewRole.Visible = true;
+                NewRoletxt.Visible = true;
+                SubmitButton.Visible = true;
+            }
+        }
+
+        protected void StatusButton_Click(object sender, EventArgs e)
+        {
+            int ID = Int32.Parse(Session["ID"].ToString());
+            int rowindex = Int32.Parse((sender as Button).CommandArgument);
+            User u = UserRepository.GetUserid(rowindex);
+            if (ID == rowindex)
+            {
+                Labelerror.Text = "Tidak bisa mengganti status diri sendiri";
+            }
+            else
+            {
+                if (u.Status == "active") 
+                {
+                    u.Status = "blocked";
+                }
+                else if (u.Status =="blocked")
+                {
+                    u.Status = "active";
+                }
+                UserRepository.ChangeUserStatus(u.ID, u.Status);
+            }
+            RefreshGridView();
+        }
+
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            int id = Int32.Parse(LabelID.Text.ToString());
+            if (NewRoletxt.Text == "")
+            {
+                Labelerror.Text = "New Role kosong";
+            }
+            else
+            {
+                int role = Int32.Parse(NewRoletxt.Text.ToString());
+                if (role == 1 || role == 2)
+                {
+                    UserRepository.ChangeUserRole(id, role);
+                    RefreshGridView();
+                    Response.Redirect("ViewUser.aspx");
+                }
+                else
+                { 
+                    Labelerror.Text = "Role yang anda input salah";
+                }
+            }
+        }
+
+        protected void HomeButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("HomePage.aspx");
+        }
+    }
+}
